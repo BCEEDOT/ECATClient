31081a66d25f5064d09a2c7a46e820ed80d857f4
DESKTOP-7892F9D\ender <enderjs>
2017-11-09 12:19:55 -0600

    --Moved LTI error handling to web services.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Subscriber, Subscription } from 'rxjs/Rx';
-import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { MdIconRegistry } from '@angular/material';
 import {
@@ -8,12 +8,11 @@ import {
 } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { Observer } from 'rxjs/Observer';
-import { TdLoadingService } from "@covalent/core";
+import { TdLoadingService } from '@covalent/core;
 import 'rxjs/add/operator/distinctUntilChanged';
 import 'rxjs/add/operator/groupBy';
 import 'rxjs/add/operator/mergeAll';
 
-import { TdDialogService } from '@covalent/core';
 import { GlobalService, ILoggedInUser } from './core/services/global.service';
 import { Person } from './core/entities/user/person';
 import { AuthService } from './core/services/auth.service';
@@ -23,7 +22,7 @@ import { AuthService } from './core/services/auth.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, AfterViewInit {
+export class AppComponent implements OnInit {
 
   persona: ILoggedInUser;
   isFaculty: boolean = false;
@@ -37,8 +36,7 @@ export class AppComponent implements OnInit, AfterViewInit {
     private loadingService: TdLoadingService,
     private _domSanitizer: DomSanitizer,
     private global: GlobalService,
-    private authService: AuthService,
-    private dialogService: TdDialogService) {
+    private authService: AuthService) {
 
     router.events.
       filter((e: Event) => isStart(e) || isEnd(e))
@@ -89,47 +87,6 @@ export class AppComponent implements OnInit, AfterViewInit {
         }
       }
     });
-
-  }
-
-  ngAfterViewInit(): void {
-    let accessTokenSigned = localStorage.getItem('ecatAccessToken');
-    if (accessTokenSigned === "undefined") {
-      let lsError = localStorage.getItem('ecatError');
-      if (lsError !== "undefined") {
-        let parseError = JSON.parse(lsError);
-        this.dialogService.openAlert({
-          message: parseError["error_description"], 
-          title: parseError.error});
-        return null;
-      } else {
-        console.log('Error: No token in localStorage, but no Lti Error either.');
-        return null;
-      }
-      
-    }
-    // there has to be a better way to do this
-    // TODO: Implement error handling
-    // if (this.tokenRef.nativeElement.ownerDocument.body.children[0].tagName === 'ECAT-APP') {
-    //   // if we aren't coming through LTI the first child seems to be the ECAT-APP element that contains the angular version
-    //   return;
-    // }
-
-    // let viewBag: string = this.tokenRef.nativeElement.ownerDocument.body.children[0].attributes[1].value;
-
-    // if (viewBag !== '') {
-    //   let vBData = JSON.parse(viewBag);
-    //   localStorage.setItem('ecatAccessToken', vBData.access_token);
-    //   localStorage.setItem('ecatUserIdToken', vBData.id_token);
-    // } else {
-    //   // if the first attribute is empty it means we had an error, which will be in the second
-    //   viewBag = this.tokenRef.nativeElement.ownerDocument.body.children[0].attributes[2].value;
-    //   this.dialogService.openAlert({
-    //     message: 'There was an error logging you in. ' + viewBag,
-    //     title: 'Please try again',
-    //     // closeButton: 'Dismiss'
-    //   });
-    // }
 
   }
 
